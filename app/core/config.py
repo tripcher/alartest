ea5851bec963f3911c2e4d95922280ba62a6db5b
s.shkitin <s.shkitin@bestdoctor.ru>
2022-03-11 10:39:25 +0700

    Fix host in config.

--- app/core/config.py
+++ app/core/config.py
@@ -11,15 +11,15 @@ class Settings(BaseSettings):
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
-    SERVER_HOST: AnyHttpUrl = "http://127.0.0.1"
+    SERVER_HOST: str = "127.0.0.1"
     SERVER_PORT: int = 8000
-    BASE_API_URL: Optional[str] = None
+    BASE_API_URL: Optional[AnyHttpUrl] = None
 
     @validator("BASE_API_URL", pre=True)
     def assemble_base_url(
         cls, v: Optional[str], values: Dict[str, Any]
     ) -> str:
-        return f'{values.get("SERVER_HOST", "")}:{values.get("SERVER_PORT", "")}{values.get("API_V1_STR", "")}'
+        return f'http://{values.get("SERVER_HOST", "")}:{values.get("SERVER_PORT", "")}{values.get("API_V1_STR", "")}'
 
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '['http://localhost', 'http://localhost:4200', 'http://localhost:3000', \
